634e2d1b65e3a902c3a7c4e4bfa7331a7aa051f1
Guptaabhii <112747867+Guptaabhii@users.noreply.github.com>
2024-12-28 00:04:19 +0530

    implemented login api

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,4 +1,58 @@
+import { useState } from "react"
+
+import axios from 'axios';
 
 export const Login = () => {
-    return <div>Login</div>
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("")
+
+    const handleClick = async () => {
+
+        try {
+
+            const res = await axios.post("http://localhost:3000/login", { email, password }, { withCredentials: true })
+
+        } catch (e) {
+            console.log(e)
+        }
+
+
+    }
+
+
+    return <>
+        <div className="card bg-neutral text-neutral-content w-96">
+            <div className="card-body items-center text-center">
+                <h2 className="card-title">LOGIN PAGE</h2>
+
+                <label className="form-control w-full max-w-xs">
+                    <div className="label">
+                        <span className="label-text">Email</span>
+
+                    </div>
+                    <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" value={email} onChange={(e) => {
+                        setEmail(e.target.value)
+                    }} />
+
+                </label>
+                <label className="form-control w-full max-w-xs">
+                    <div className="label">
+                        <span className="label-text">Password</span>
+
+                    </div>
+                    <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" value={password} onChange={(e) => {
+                        setPassword(e.target.value)
+                    }} />
+
+                </label>
+
+                <div className="card-actions justify-end">
+                    <button className="btn btn-primary" onClick={handleClick}>Login</button>
+                </div>
+            </div>
+        </div>
+    </>
+
+
 }
