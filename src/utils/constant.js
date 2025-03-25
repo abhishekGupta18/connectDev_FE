bf07515af6d907d0f9fb17b01754f9e41933083c
Abhishek Gupta <112747867+abhishekGupta18@users.noreply.github.com>
2025-03-25 14:44:07 +0530

    Update constant.js

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,4 @@
 export const baseURL =
   location.hostname === "localhost"
     ? "http://localhost:3000"
-    : "http://connectdev-8rpcn1ftp-abhishekgupta18s-projects.vercel.app/";
+    : "http://connectdev-8rpcn1ftp-abhishekgupta18s-projects.vercel.app";
