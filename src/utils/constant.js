567d9c12a39b42b013a320200ee1622ce0df66fe
Guptaabhii <112747867+Guptaabhii@users.noreply.github.com>
2025-03-25 14:41:21 +0530

    updated constant.js

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,2 +1,4 @@
 export const baseURL =
-  location.hostname === "localhost" ? "http://localhost:3000" : "/api";
+  location.hostname === "localhost"
+    ? "http://localhost:3000"
+    : "http://connectdev-8rpcn1ftp-abhishekgupta18s-projects.vercel.app/";
