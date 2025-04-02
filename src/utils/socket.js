1ba4759a1f7aedb833a99377c0f81cd1ebcd87c1
Guptaabhii <112747867+Guptaabhii@users.noreply.github.com>
2025-04-02 15:20:50 +0530

    config socket.js

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,9 +2,5 @@ import io from "socket.io-client";
 import { baseURL } from "./constant";
 
 export const createSocketConnection = () => {
-  if (location.hostname == "localhost") {
-    return io(baseURL);
-  } else {
-    return io("/", { path: "/api/socket.io" });
-  }
+  return io(baseURL);
 };
